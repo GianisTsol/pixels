8dc81d7a9fc74a5a665d61cd08370ff2e8ab40be
GianisTsol <giannisetsolakis@gmail.com>
2021-05-31 20:44:59 +0300

    works.

--- main.py
+++ main.py
@@ -19,6 +19,10 @@ church_headers = {"Authentication": f"{keys[1]}"}
 HEADERS = {"Authorization": f"Bearer {keys[0]}"}
 data = {"x": 123,"y": 12,"rgb": "87CEEB",}
 
+remaining = 0
+
+task = {}
+
 def sendpixel(x, y, rgb):
     global data
     data['x'] = int(x)
@@ -42,22 +46,31 @@ def sendpixel(x, y, rgb):
     global reset
     reset = float(info["requests-reset"])
 
+def check_remaining():
+    global remaining
+    result = requests.head("https://pixels.pythondiscord.com/set_pixel",headers=HEADERS)
 
-result = requests.head("https://pixels.pythondiscord.com/set_pixel",headers=HEADERS)
-remaining = result.headers['Requests-Remaining']
+    if "requests-remaining" in result.headers:
+        remaining = result.headers['Requests-Remaining']
+    if "cooldown-reset" in result.headers:
+        print("Set pixel cooldown reset. ", int(result.headers["cooldown-reset"]))
+        time.sleep(int(result.headers["cooldown-reset"]))
+    if remaining == 0:
+        print("waiting... " + str(reset))
+        time.sleep(reset)
+        remaining = result.headers['Requests-limit']
 
 while True:
     r = requests.get("http://churchofpepe.ddns.net:5000/get_task",headers=church_headers)
     try:
-        task = r.json()
+        task = json.loads(r)
     except:
+        task = {}
         print("No tasks available! - sleeping...")
         time.sleep(20)
-    if remaining == 0:
-        print("waiting... " + str(reset))
-        time.sleep(reset)
-        result = requests.head("https://pixels.pythondiscord.com/set_pixel",headers=HEADERS)
-        remaining = result.headers['Requests-Remaining']
+
+    check_remaining()
+
     print(f"Setting pixel { task['x'] }, { task['y'] } with color { task['color'] } for project { task['source'] }")
     sendpixel(task["x"], task["y"], task["color"])
     church_headers["task-id"] = str(task["id"])
